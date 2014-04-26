93260cb2beee4510c3815d5d22edda8c955542dc
sclark39 <sclark@freolic.com>
2014-04-26 13:04:18 -0700

    Adding a font.

--- src/scenes/IntroScene.lua
+++ src/scenes/IntroScene.lua
@@ -5,19 +5,39 @@ function IntroScene.OnLoad()
 	layer:setViewport( viewport )
 	MOAISim.pushRenderPass( layer )
 	
-	local bg = Utility.NewSingleUseProp( "gfx/title.png", screenw, screenh );		
-	local press_start = Utility.NewSingleUseProp( "gfx/press_start.png", 303, 39 );		
-	
+	local bg = Utility.NewSingleUseProp( "gfx/title.png", screenw, screenh );				
 	bg:setLoc( 0, 0 );
 	bg:setColor( 0, 0, 0, 0 );
-	press_start:setLoc( 0, -222 );
-	press_start:setColor( 0, 0, 0, 0 );
-
+	
 	layer:insertProp( bg );
-	layer:insertProp( press_start );
 	
 	MOAICoroutine.blockOnAction( bg:seekColor( 1, 1, 1, 1, 0.25 ) )
-		
+	
+	local font = MOAIFont.new ()
+	font:load ( "fnt/OLondon_.otf" )
+	local charcodes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 .,:;!?()&/-';
+	font:preloadGlyphs ( charcodes, 48 );
+ 
+	local style = MOAITextStyle.new ()
+	style:setFont ( font )
+	style:setSize ( 48 )
+	style:setColor ( 224/255, 199/255, 150/255, 1 )
+	
+	textbox = MOAITextBox.new();
+	textbox:setStyle( style );
+	textbox:setString( "Click to Start" );
+	
+	do
+		local w,h,x,y = 500,100,0,-222;
+		textbox:setRect ( -w/2 + x, -h/2 + y, w/2 + x, h/2 + y );
+	end
+	textbox:setYFlip(true);
+	textbox:setAlignment( MOAITextBox.CENTER_JUSTIFY, MOAITextBox.CENTER_JUSTIFY )
+	textbox:revealAll()	
+	textbox:setColor( 0, 0, 0, 0 );
+	
+	layer:insertProp( textbox )
+
 	local curve = MOAIAnimCurve.new();
 	curve:reserveKeys( 3 );
 	curve:setKey ( 1, 0, 0 )
@@ -27,10 +47,10 @@ function IntroScene.OnLoad()
 	
 	anim = MOAIAnim.new();
 	anim:reserveLinks( 4 );
-	anim:setLink( 1, curve, press_start, MOAIProp2D.ATTR_R_COL );
-	anim:setLink( 2, curve, press_start, MOAIProp2D.ATTR_G_COL );
-	anim:setLink( 3, curve, press_start, MOAIProp2D.ATTR_B_COL );
-	anim:setLink( 4, curve, press_start, MOAIProp2D.ATTR_A_COL );
+	anim:setLink( 1, curve, textbox, MOAITextBox.ATTR_R_COL );
+	anim:setLink( 2, curve, textbox, MOAITextBox.ATTR_G_COL );
+	anim:setLink( 3, curve, textbox, MOAITextBox.ATTR_B_COL );
+	anim:setLink( 4, curve, textbox, MOAITextBox.ATTR_A_COL );
 	anim:setMode( MOAITimer.LOOP );
 	anim:start();
 end
